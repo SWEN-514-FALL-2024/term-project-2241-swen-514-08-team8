e4cd8df5cdbf21871ed6d33c32cc0ac921a04158
nms4174 <69879196+nms4174@users.noreply.github.com>
2024-10-07 20:12:04 -0400

    Change main.tf to integrate with fakestoreapi (WIP)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,176 +1,78 @@
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"
 }
 
-locals {
-    aws_key = "My-AWS-KEY"   # Change this to your desired AWS region
-  }
-
-# Security Group
-resource "aws_security_group" "vpc-web" {
-  name        = "vpc-web"
-  description = "VPC web"
-  ingress {
-    description = "Allow Port 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow Port 443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all ip and ports outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_api_gateway_rest_api" "fakestore_api" {
+  name = "EcommerceAPI"
+  description = "API Gateway for routing traffic to the Fakestore API and other routes"
 }
 
-resource "aws_instance" "my_server" {
-   ami           = data.aws_ami.amazonlinux.id
-   instance_type = var.instance_type
-   key_name      = "${local.aws_key}"                  
-   user_data = file("terraform_install.sh")
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.fakestore_api.id
+  parent_id   = aws_api_gateway_rest_api.fakestore_api.root_resource_id
+  path_part   = "ecomstore"
+}
 
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.fakestore_api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
 
-   tags = {
-     Name = "my ec2"
-   }
+resource "aws_api_gateway_method" "cors_options" {
+  rest_api_id   = aws_api_gateway_rest_api.fakestore_api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
 
-   vpc_security_group_ids = [aws_security_group.vpc-web.id]                    
- }
+resource "aws_api_gateway_integration" "fakestore_integration" {
+  rest_api_id = aws_api_gateway_rest_api.fakestore_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.get_method.http_method
 
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/24"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+  type        = "HTTP"
+  uri         = "https://fakestoreapi.com/products" # Fakestore API URL
 
-  tags = {
-    Name = "VPC"
+  integration_http_method = "GET"
+}
+
+resource "aws_api_gateway_integration_response" "cors_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.fakestore_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.cors_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
 
-# Public Subnet
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.0/26"
-  map_public_ip_on_launch = true
+resource "aws_api_gateway_method_response" "cors_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.fakestore_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.cors_options.http_method
+  status_code = "200"
 
-  tags = {
-    Name = "PublicSubnet"
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
 
-# Public Subnet 2
-resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.64/26"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "PublicSubnet2"
-  }
-}
-
-# Private Subnet
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.128/26"
-
-  tags = {
-    Name = "PrivateSubnet"
-  }
-}
-
-# Private Subnet 2
-resource "aws_subnet" "private2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.192/26"
-
-  tags = {
-    Name = "PrivateSubnet2"
-  }
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "InternetGateway"
-  }
-}
-
-# NAT Gateway
-resource "aws_eip" "ngw" { 
-  domain = "vpc"
-  instance = aws_instance.my_server.id
-}
-
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ngw.id
-  subnet_id = aws_subnet.public.id
-
-  tags = {
-    Name = "NATGateway"
-  }
-}
-
-# Route Table for Public Subnet
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "PublicRouteTable"
-  }
-}
-
-# Route Table for Private Subnet
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.main.id
-  
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "PrivateRouteTable"
-  }
-}
-
-# Associate Route Table with Public Subnets
-resource "aws_route_table_association" "public_association" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-resource "aws_route_table_association" "public_association_2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-# Associate Route Table with Private Subnets
-resource "aws_route_table_association" "private_association" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private_route_table.id
-}
-
-resource "aws_route_table_association" "private_association_2" {
-  subnet_id      = aws_subnet.private2.id
-  route_table_id = aws_route_table.private_route_table.id
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [aws_api_gateway_integration.fakestore_integration]
+  rest_api_id = aws_api_gateway_rest_api.fakestore_api.id
+  stage_name  = "test"
 }
