258c2745b86070f4c3ef1f8a23f56fd581d292ca
Emmet Spencer <emmetwormbox6@gmail.com>
2024-10-28 11:01:30 -0400

    comment out lambdas, moving to another branch for incremental commits

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,34 +1,44 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
+#     }
 
-    actions = ["sts:AssumeRole"]
-  }
-}
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
 
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name               = "iam_for_lambda"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
+# # Modify this variable to include additional lambda files.
+# variable "lambda_functions" {
+#   default = {
+#     "get_products"    = "get_products.lambda_handler"
+#     "create_product"    = "create_product.lambda_handler"
+#   }
+# }
 
-data "archive_file" "zip_the_python_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/python/"
-  output_path = "${path.module}/python/hello-python.zip"
-}
+# data "archive_file" "zips" {
+#   for_each    = var.lambda_functions
+#   type        = "zip"
+#   source_content_filename = "${path.module}/../lambdas/${each.key}.py"
+#   output_path = "${path.module}/../lambdas/zips/${each.key}.zip"
+# }
 
-resource "aws_lambda_function" "test_lambda" {
-  function_name = "get_product_function"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "get_products.lambda_handler"
-  runtime       = "python3.12"
-  filename = "${path.module}/../lambdas/get_products.zip"
-  
-  depends_on = [ data.archive_file.zip_the_python_code ]
-}
+# # Create the lambda function.
+# resource "aws_lambda_function" "lambda" {
+#   for_each      = var.lambda_functions
+#   function_name = "ecom_lambda_${each.key}"
+#   role          = aws_iam_role.iam_for_lambda.arn
+#   handler       = each.value
+#   runtime       = "python3.12"
+#   filename      = data.archive_file.zips[each.key].output_path
+
+#   depends_on = [ aws_iam_role.iam_for_lambda ]
+# }
