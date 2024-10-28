5159a98bdb2b70354a0cf7bbe7a7c92c75e09924
Emmet Spencer <emmetwormbox6@gmail.com>
2024-10-28 12:16:00 -0400

    consolidate product lambdas, add api_gateway_url in outputs

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,44 +1,29 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-# Modify this variable to include additional lambda files.
-variable "lambda_functions" {
-  default = {
-    "get_products"    = "get_products.lambda_handler"
-    "create_product"    = "create_product.lambda_handler"
-  }
-}
-
+# To upload lambda functions in terraform, we need to zip them first.
+# It will be easier to upload all of our lambda functions if we group similar lambda functions in the same python
 data "archive_file" "zips" {
-  for_each    = var.lambda_functions
   type        = "zip"
-  source_content_filename = "${path.module}/../lambdas/${each.key}.py"
-  output_path = "${path.module}/../lambdas/zips/${each.key}.zip"
+  source_file = "${path.module}/../lambdas/products.py"
+  output_path = "${path.module}/../lambdas/zips/products.zip"
 }
 
-# Create the lambda function.
-resource "aws_lambda_function" "lambda" {
-  for_each      = var.lambda_functions
-  function_name = "ecom_lambda_${each.key}"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = each.value
+# get-products
+resource "aws_lambda_function" "get-products" {
+  function_name = "ecommerce-get-products"
+  role          = aws_iam_role.lambda_iam_role.arn
+  handler       = "products.get_products"
   runtime       = "python3.12"
-  filename      = data.archive_file.zips[each.key].output_path
+  filename      = "${path.module}/../lambdas/zips/products.zip"
 
-  depends_on = [ aws_iam_role.iam_for_lambda ]
+  depends_on = [ aws_iam_role.lambda_iam_role ]
+}
+
+# create-product  
+resource "aws_lambda_function" "create-products" {
+  function_name = "ecommerce-create-products"
+  role          = aws_iam_role.lambda_iam_role.arn
+  handler       = "products.create_product"
+  runtime       = "python3.12"
+  filename      = "${path.module}/../lambdas/zips/products.zip"
+
+  depends_on = [ aws_iam_role.lambda_iam_role ]
 }
